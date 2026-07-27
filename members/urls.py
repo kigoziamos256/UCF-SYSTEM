faa52cae301d4fea80938c67a52b0aaed690deed
kigoziamos256 <kigoziamos25@gmail.com>
2026-07-27 15:37:29 -0700

    Update urls.py

--- members/urls.py
+++ members/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', views.custom_logout, name='logout'),
     
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('profile/', views.profile_view, name='profile'),
