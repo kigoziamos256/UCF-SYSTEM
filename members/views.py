a857f2b811eb9d6f6ffd7e0d77696eeecf3f3828
kigoziamos256 <kigoziamos25@gmail.com>
2026-08-13 04:00:43 -0700

    Update views.py

--- members/views.py
+++ members/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import login, logout
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
+from django.db.models import Q, Sum, Count
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.contrib import messages
